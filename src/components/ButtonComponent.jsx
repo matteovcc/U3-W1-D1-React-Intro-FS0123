0b750d7eeb98375e39350b9e7909dd95a4145835
matteovcc <matteovcc99@gmail.com>
2023-03-27 16:18:03 +0200

    edit

--- src/components/ButtonComponent.jsx
+++ src/components/ButtonComponent.jsx
@@ -1,7 +1,7 @@
 const ButtonComponent = props =>{
     return(
         <>
-        <button style={{backgroundColor:props.bgColor}}>{props.content}</button>
+        <button style={{backgroundColor:props.bgColor,color:props.color,padding:props.padding, border:props.border,margin:props.margin,borderRadius:props.radius,fontSize:props.size}}>{props.content}</button>
         </>
     )
 }
